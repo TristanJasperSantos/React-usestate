d5c0438ce2a2c96d94cdf31320dc56e85b5591bb
Renz Santos <tristan456_6@live.com>
2022-12-29 11:19:49 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = ()=>{
+    const [count, setCount] = useState(0);
+
+    const minusCount = ()=>{
+      setCount(prevCount=>{
+         return prevCount - 1;
+      })
+    }
+
+    const addCount = ()=>{
+      setCount (prevCount =>{
+        return prevCount + 1;
+      }) 
+    }
+
+  return(
+      <div>
+        <div className=' flex justify-center items-center w-80 h-80 rounded-full bg-white mb-4'>
+          <h1 className=' text-7xl'>{count}</h1>
+        </div>
+        <div className=' flex gap-8 justify-center items-center text-white'>
+          <button onClick={minusCount} className=' flex justify-center items-center p-6 h-4 w-4 rounded-full border border-white text-2xl'>-</button>
+          <button onClick={addCount} className=' flex justify-center items-center p-6 h-4 w-4 rounded-full border border-white text-2xl'>+</button>
+        </div>
+      </div>
+  )
 }
 
 export default App;
